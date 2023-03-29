0b7e728cee4e26a2e4824b041de24b64c3accb67
Pablo <pablosdurbano@gmail.com>
2023-03-29 10:01:58 +0200

    Exercise 3

--- src/context/contextStarship.jsx
+++ src/context/contextStarship.jsx
@@ -7,19 +7,35 @@ export const StarsContext = createContext();
 export const StarsContextProvider = (props) => {
   // Estado para guardar las StarShips
   const [Ships, setShips] = useState([]);
+  // Estado para guardar la página actual
+  const [page, setPage] = useState(1);
 
-  // UseEffect para obtener los datos de la API al cargar la página
-  useEffect(() => {
-    fetch("https://swapi.dev/api/starships/")
-      .then((res) => res.json())
-      .then((data) => {
-        // Guardamos las StarShips en el estado
-        setShips(data.results);
-      })
-      .catch((error) => {
-        console.error("Error", error);
-      });
-  }, []);
+// UseEffect para obtener los datos de la API al cargar la página
+useEffect(() => {
+  fetch(`https://swapi.dev/api/starships/?page=${page}`)
+    .then((res) => res.json())
+    .then((data) => {
+      // Guardamos las StarShips en el estado
+      setShips((prevShips) => [...prevShips, ...data.results]);
+    })
+    .catch((error) => {
+      console.error("Error", error);
+    });
+}, [page]);
+
+// UseEffect para agregar el evento de scroll
+useEffect(() => {
+  window.addEventListener("scroll", handleScroll);
+  return () => window.removeEventListener("scroll", handleScroll);
+}, []);
+
+// Función para cargar más datos cuando se llega al final de la página
+const handleScroll = () => {
+  const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight) {
+    setPage((prevPage) => prevPage + 1);
+  }
+};
 
   // Renderizamos el componente App dentro del contexto de las StarShips
   return (
